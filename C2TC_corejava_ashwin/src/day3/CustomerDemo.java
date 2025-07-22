5a7836b842ff51cc76f8ab0466df786738b820d3
Ashwin-wrk <ashwinwrk1204@gmail.com>
2025-07-22 23:24:14 +0530

    Assignment

---
+++ C2TC_corejava_ashwin/src/day3/CustomerDemo.java
@@ -0,0 +1,14 @@
+package day3;
+
+public class CustomerDemo {
+
+	public static void main(String[] args) {
+		Customer c1=new Customer();
+		c1.setCid(101);
+		c1.setCname("Ash");
+		c1.setCity("Chennai");
+		System.out.println(c1);
+
+	}
+
+}
