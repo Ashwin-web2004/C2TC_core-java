ca01d32d447ec3ee9b04750ea02cb60fb65cbfc5
Ashwin-wrk <ashwinwrk1204@gmail.com>
2025-07-25 00:03:17 +0530

    Day 4 and 5

---
+++ C2TC_corejava_ashwin/src/day5/MultilevelInheritance/TextEditor/Notepad.java
@@ -0,0 +1,28 @@
+package day5.MultilevelInheritance.TextEditor;
+
+public class Notepad {
+	protected String content;
+
+    public Notepad() {
+        this.content = "";
+    }
+
+    public void write(String text) {
+        content += text; //content = content+text
+    }
+
+    public void displayContent() {
+        System.out.println("Notepad Content: " + content);
+    }
+    
+    
+    /*
+     * returntype funcName()
+     * {
+     * 		//statement
+     * }
+     * 
+     * */
+
+
+}
